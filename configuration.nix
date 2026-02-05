a3c1bcc24cd54297d36b983384775717c27bc41c
Jason Shi <shibingzhao@outlook.com>
2026-02-05 03:10:04 +0000

    remove nvidia

--- configuration.nix
+++ configuration.nix
@@ -303,13 +303,13 @@
 
   # IMPORTANT!
   # nVIDIA -- TODO: REMOVE IF NOT USING NVIDIA
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia.open = true;
-  hardware.nvidia.prime = {
-    intelBusId = "PCI:0@0:2:0";
-    nvidiaBusId = "PCI:1@0:0:0";
-  };
-  hardware.nvidia.modesetting.enable = true;
+  # services.xserver.videoDrivers = [ "nvidia" ];
+  # hardware.nvidia.open = true;
+  # hardware.nvidia.prime = {
+  #   intelBusId = "PCI:0@0:2:0";
+  #   nvidiaBusId = "PCI:1@0:0:0";
+  # };
+  # hardware.nvidia.modesetting.enable = true;
 
 
   # Systemd configs
