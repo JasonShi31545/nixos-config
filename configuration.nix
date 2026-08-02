cee37ce30b9d38b427a0b3c364324e4e0b8cdf7e
JasonShi31545Personal <REDACTED>
2026-08-02 17:16:12 -0400

    add openrgb

--- configuration.nix
+++ configuration.nix
@@ -418,6 +418,12 @@
     DefaultTimeoutStopSec="10s";
   };
 
+  # RGB
+  services.hardware.openrgb = {
+    enable = true;
+    motherboard = "amd";
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
